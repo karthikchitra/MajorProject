5ac8b6e77f0ad1e7fdde93f08e34e7b4b65c64b4
Karthik Chitra <karthikchitra272@gmail.com>
2025-12-12 19:16:40 +0530

    Add files via upload

---
+++ Student record/script.js
@@ -0,0 +1,10 @@
+// Variables
+let studentName = "Rahul";
+let totalMarks = 75;
+let isPassed = totalMarks > 40;
+let Boolean = true;
+
+// Output
+console.log("Student Name:", studentName);
+console.log("Total Marks:", totalMarks);
+console.log("Passed:", isPassed);
